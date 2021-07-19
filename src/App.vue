8adf41d9d9a32f30be87ed43350f20c64c8d2ccb
C01dy <faltrue7@gmail.com>
2021-07-19 17:27:39 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header
+    class="main-header is-flex is-flex-direction-column is-align-items-center"
+  >
+    <h1 class="title is-1 mt-5 ">
+      C<i class="fas fa-virus has-text-primary-dark"></i>VID-19
+      CORONAVIRUS PANDEMIC
+    </h1>
+    <span class="last-info">
+      <span
+        class="last last-reported has-background-info-light pl-3 pr-3 pt-1 pb-1 m-2 is-underlined"
+      >
+        Last reported: {{ totalNumbers.lastReported }}
+      </span>
+      <span
+        class="last last-checked has-background-info-light pl-3 pr-3 pt-1 pb-1 m-2 is-underlined"
+      >
+        Last checked: {{ totalNumbers.lastChecked }}
+      </span>
+    </span>
+  </header>
+  <section class="total-stats columns is-desktop is-vcentered">
+    <div class="total-stats__cases column">
+      <div class="card has-background-warning-dark">
+        <div
+          class="card-content is-flex is-flex-direction-column is-align-items-center"
+        >
+          <i class="fas fa-lungs-virus is-size-3 has-text-white mb-3"></i>
+          <h3 class="title is-3 has-text-white">Coronavirus Cases:</h3>
+          <h2 class="title is-2 has-text-white">
+            {{ totalNumbers.confirmed }}
+          </h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="total-stats__deaths column">
+      <div class="card has-background-danger-dark">
+        <div
+          class="card-content is-flex is-flex-direction-column is-align-items-center"
+        >
+          <i class="fas fa-skull is-size-3 has-text-white mb-3"></i>
+          <h3 class="title is-3 has-text-white">Deaths:</h3>
+          <h2 class="title is-2 has-text-white">{{ totalNumbers.deaths }}</h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="total-stats__deaths column">
+      <div class="card has-background-success">
+        <div
+          class="card-content is-flex is-flex-direction-column is-align-items-center"
+        >
+          <i class="fas fa-star-of-life is-size-3 has-text-white mb-3"></i>
+          <h3 class="title is-3 has-text-white">Recovered:</h3>
+          <h2 class="title is-2 has-text-white">
+            {{ totalNumbers.recovered || '-' }}
+          </h2>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { getTotalNumbers } from './api';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      topMovieList: [],
+      totalNumbers: {},
+    };
+  },
+  methods: {},
+  mounted() {
+    getTotalNumbers().then((res) => (this.totalNumbers = res.data));
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  font-family: 'Cabin', sans-serif;
+}
+
+h1,
+h2,
+h3 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.total-stats {
+  height: 80vh;
+  margin: 0 1.5em;
 }
 </style>
---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+export async function fetchDataFromAPI(url = '') {
+  const rootURL = 'https://covid-19-coronavirus-statistics.p.rapidapi.com/v1';
+  const res = await fetch(rootURL + url, {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': '259c09283bmsh08fcc5b2f200a24p100809jsna779d603535d',
+      'x-rapidapi-host': 'covid-19-coronavirus-statistics.p.rapidapi.com',
+    },
+  });
+  return res.json();
+}
+
+export async function getTotalNumbers() {
+  return await fetchDataFromAPI('/total');
+}
+
+export async function getAllCountriesStats() {
+  return await fetchDataFromAPI('/stats');
+}
